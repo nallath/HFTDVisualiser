b49c1ccad19468956d4c99594fd8c48e817d952a
Jaime van Kessel <nallath@gmail.com>
2020-03-28 21:15:33 +0100

    Fix typo
    whoops.

--- visualise.py
+++ visualise.py
@@ -237,7 +237,7 @@ def createUML(ports, trace_routes):
 
     result = ""
     result += "@startuml\n"
-    result += "title\nProudly powered by\n <size:30><b>SeriOusBusiness_S</b>™℠®© Pat. Pend.</size>\n &\n <size:30>PandaPosse Inc.™℠®©<size>\nendtitle\n"
+    result += "title\nProudly powered by\n <size:30><b>SeriOusBusiness_S</b>™℠®© Pat. Pend.</size>\n &\n <size:30>PandaPosse Inc.™℠®©</size>\nendtitle\n"
     result += "skinparam backgroundColor #000000\n"
     result += "skinparam databaseBorderColor #00FF00\n"
     result += "skinparam nodeBorderColor #00FF00\n"
